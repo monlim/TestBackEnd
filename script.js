1625ac0d430b99638280b727f6aa2409f73fbae7
monlim <71799456+monlim@users.noreply.github.com>
2023-07-02 20:39:42 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const getBtn = document.getElementById("get");
+const postBtn = document.getElementById("post");
+const input = document.getElementById("input");
+
+const baseURL = "http://localhost:8383/";
+
+getBtn.addEventListener("click", getInfo);
+postBtn.addEventListener("click", postInfo);
+
+async function getInfo(e) {
+  e.preventDefault();
+  const res = await fetch(baseURL + "info/james?key=hello", {
+    method: "GET",
+  });
+  console.log(res);
+  const data = await res.json();
+  input.value = data.info;
+}
+async function postInfo(e) {
+  e.preventDefault();
+  if (input.value == "") {
+    return;
+  }
+  const res = await fetch(baseURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      parcel: input.value,
+    }),
+  });
+}
